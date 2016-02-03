678f57726e54717360535d87ad6567b49a9f2e0a
David Cosman <dcosman@uwo.ca>
2016-02-03 18:45:27 -0500

    Create constants.h

---
+++ Asn1/constants.h
@@ -0,0 +1,15 @@
+/*
+ * Common constants used with shell
+ */
+
+#ifndef constants_h
+#define constants_h 
+
+#define MAX 256
+#define CMD_MAX 10
+#define MAX_HISTORY 10
+
+#define READ_PIPE  0
+#define WRITE_PIPE 1
+
+#endif
