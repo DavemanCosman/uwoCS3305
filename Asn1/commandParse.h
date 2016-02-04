e5c48067129d2fecc13f5d808df448943f6fa635
David Cosman <dcosman@uwo.ca>
2016-02-03 19:50:18 -0500

    Update commandParse.h

--- Asn1/commandParse.h
+++ Asn1/commandParse.h
@@ -4,10 +4,7 @@
 #include "bool.h"
 #include "commandType.h"
 
-// parse a single command
-void parse_command (char * token, struct commandType *comm);
-
 // parse commandline for space separated commands
-void parse_tokens(char *buf, char *tokens[]);
+void parse_tokens (char *buf, struct commandType* comm);
 
 #endif
