c06367673c89e23e66a75ac96f5f4227ddc8fe48
David Cosman <dcosman@uwo.ca>
2016-02-04 16:33:21 -0500

    Create testShell.c
    Shell to test commandParse.c

---
+++ Asn1/testShell.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "commandType.h"
+#include "constants.h"
+#include "bool.h"
+#include "commandParse.h"
+
+void main (void)
+{
+	char input_line[LINE_MAX];
+	char* command_line = input_line;
+
+	printf("DavidCosman> ");
+	fgets(input_line, LINE_MAX, stdin);
+	struct commandType command = {
+		.line = command_line
+	};
+	commandParse (command);
+	
+	int i = 0;
+	while ((*command).args[i] != NULL) {
+		printf("extracted arg is %s\n",(*command).args[i]);
+	}
+	printf("\nHello World\n");
+
+}
