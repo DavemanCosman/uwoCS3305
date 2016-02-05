a546b47b852c88eccb1df390c45f704d2868302f
David Cosman <dcosman@uwo.ca>
2016-02-04 21:19:57 -0500

    Update dcosmanShell.c
    Added template

--- Asn1/dcosmanShell.c
+++ Asn1/dcosmanShell.c
@@ -7,13 +7,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#include "command.h"
+#include "commandType.h"
 #include "constants.h"
+#include "commandParse.h"
+#include "bool.h"
 
 typedef void (*sighandler_t)(int);
-
-// Array will hold arguments
-static char* args[MAX_CMD];
 
 void handle_signal(int signo)
 {
@@ -30,27 +29,29 @@ void fill_argv(char *tmp_argv)
 
 int main(int argc, char const *argv[])
 {
-	char input_line[MAX];
-	int i, n;
-	signal(SIGINT, SIG_IGN);
-	signal(SIGINT, handle_signal);
+	char input_line[LINE_MAX];
+	char* command_line = input_line;
 	
-	printf("\nDavidCosman> ");
-	if (fgets(input_line, MAX, stdin) != NULL)
-		n = make_tokenlist(input_line, tokens);
-	else
-		printf("Command not found\n");
-	while(c != EOF) {
-		c = getchar();
-		switch(c) {
-			case '\n': /* parse and execute */
-				bzero(tmp, sizeof(tmp));
-				break;
-			default: strncat(tmp, &c, 1);
-				break;
-		}
-		printf("DavidCosman> ");
+	// initalize history here
+	struct commandType command;
+
+	printf("/****************************************/ \n");
+	printf("/* Welcome to Dcosman Shell		*/ \n");
+	printf("/* exit : quit shell			*/ \n");
+	printf("/* history : display last 10 commands	*/ \n");
+	printf("/****************************************/ \n");
+	for (;;) {
+		// Prompt
+		printf("\nDavidCosman> ");
+		
+		// Read command and give to history
+		fgets(input_line, LINE_MAX, stdin);
+		// put history item here
+		
+		command.line = command_line;
+		commandParse (&command);
 	}
 	printf("\n");
+	// clean up here
 	return 0;
 }
