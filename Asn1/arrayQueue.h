0f36b48a0b6e85ab9daf8d6ec3a4dc55c496308f
David Cosman <dcosman@uwo.ca>
2016-02-05 15:33:14 -0500

    Update and rename arrayList.h to arrayQueue.h
    A queue structure would be more useful

---
+++ Asn1/arrayQueue.h
@@ -0,0 +1,40 @@
+#ifndef queueList_h
+#define queueList_h
+
+#include <stddef.h>
+
+/* arrayQueue
+ * Implement a circular queue using an array
+ */
+struct arrayQueue
+{
+	// rear and front node indexes
+	int rear = - 1;
+	int front = - 1;
+	// The current size
+	int size;
+	// The array of elements in the queue
+	const void** queue_array_elements;
+}
+
+typedef struct arrayQueue Queue;
+
+// initializes queue; sets it empty at an initial capacity
+void init_queue (Queue** queue, int capacity, size_t elementSize);
+
+// adds node to back of queue
+void addToQueue (Queue* queue, const void* node);
+
+// deletes front item from queue
+void removeFromQueue (Queue* queue);
+
+// returns the queue array elements
+const void* displayQueue (Queue* list);
+
+// frees memory used by array queue
+void free_Queue(Queue** list);
+
+// frees all elements in array queue
+void free_QueueElements(Queue* list);
+
+#endif
