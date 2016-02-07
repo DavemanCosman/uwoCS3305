e9b371f392d4c9f1e0502b2277e80744fc50e429
David Cosman <dcosman@uwo.ca>
2016-02-07 16:45:50 -0500

    Create commandExecute.h

---
+++ Asn1/commandExecute.h
@@ -0,0 +1,10 @@
+#ifndef commandExecute_h
+#define commandExecute_h
+
+#include "commandType.h"
+
+// Executes the given command in the current context. 
+// Handles the chaining of commands via pre-existing pipes and IO redirection. 
+void execute_command( struct command_t* command, int* pipes, int input_fd );
+
+#endif
