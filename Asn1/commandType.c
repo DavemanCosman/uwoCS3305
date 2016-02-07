e9fe2109f4c519caf5f54cee827d93297a08a806
David Cosman <dcosman@uwo.ca>
2016-02-07 13:14:46 -0500

    Update commandType.c

--- Asn1/commandType.c
+++ Asn1/commandType.c
@@ -23,13 +23,13 @@ static void wait_on_forks()
 static int runCommand ( struct commandType* command, int input_fd )
 {
 	++fork_count;
-
 	int pipes[2];
 	pipe(pipes);	
 
 	pid_t pid = fork();
 	// if child process
-	if (pid == 0) {
+	if (pid == 0) { 
+		//execute command here
 		execute_command (command, pipes, input_fd);
 	}
  
@@ -72,15 +72,13 @@ static inline void historyCommand( struct commandType* command )
 static bool handle_internal_command( struct commandType* command )
 {
 	// exit
-	if (strcmp((*command).args[0], "exit" ) == 0) 
-	{
+	if (strcmp((*command).args[0], "exit" ) == 0) {
 		exitCommand(command);
 		return true;
 	}
 
 	// history
-	if (strcmp((*command).args[0], "history" ) == 0 ) 
-	{
+	if (strcmp((*command).args[0], "history" ) == 0) {
 		historyCommand(command);
 		return true;
 	}
@@ -91,13 +89,10 @@ static bool handle_internal_command( struct commandType* command )
 
 int run( struct commandType* command, int input )
 {
-	parse_command( command );
-
-	if ( command->command != NULL ) 
-	{
-		if( !handle_internal_command( command ) )
-		{
-			return runCommand( command, input );			
+	commandParse (command);
+	if ((*command).line != NULL) {
+		if(!handle_internal_command(command)) {
+			return runCommand(command, input);			
 		}
 	}
 	return 0;
