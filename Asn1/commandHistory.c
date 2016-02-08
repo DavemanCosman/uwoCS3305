e83f837cc7f90b37359806b206ff56d512acb906
David Cosman <dcosman@uwo.ca>
2016-02-08 12:49:35 -0500

    Comments and descriptions

--- Asn1/commandHistory.c
+++ Asn1/commandHistory.c
@@ -11,11 +11,17 @@
 
 static Queue* history;
 
+/* init_commandHistory
+ * Initialize command history memory storage using arrayQueue
+ */
 void init_commandHistory()
 {
 	init_queue (&history, MAX_HISTORY, sizeof(char) * LINE_MAX );
 }
 
+/* create_commandHistory_entry
+ * allocate memory for new history entry
+ */
 char* create_commandHistory_entry (char* command)
 {
 	char* history_entry = malloc(sizeof(char) * LINE_MAX);
@@ -23,11 +29,17 @@ char* create_commandHistory_entry (char* command)
 	return history_entry;
 }
 
+/* add_commandHistory_entry
+ * add history entry to the arrayQueue
+ */
 void add_commandHistory_entry (char* history_entry)
 {
 	addToQueue (history, history_entry);
 }
 
+/* print_commandHistory
+ * display the last 10 commands
+ */
 void print_commandHistory ()
 {
 	if ((*history).front == -1) {
@@ -41,7 +53,7 @@ void print_commandHistory ()
 			history_count = (*history).rear + 1;
 		}
 	
-		// if there is only one element in history, then front == rear (both 0)
+		// if there is only one element in history, then front == rear (both index = 0)
 		if ((*history).front == (*history).rear) {
 			output = (char*) displayQueueFront(history);
 			printf("%s",output);
@@ -61,6 +73,9 @@ void print_commandHistory ()
 	}
 }
 
+/* free_commandHistory
+ * free memory taken by history
+ */
 void free_commandHistory() 
 {
 	free_QueueElements (history);
