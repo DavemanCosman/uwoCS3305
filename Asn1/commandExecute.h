14e773de56d8f2f15fea696e137823e863abdc01
David Cosman <dcosman@uwo.ca>
2016-02-08 12:30:54 -0500

    Delete commandExecute.h
     Not needed, can be put in commandType.c

--- Asn1/commandExecute.h
+++
@@ -1,9 +0,0 @@
-#ifndef commandExecute_h
-#define commandExecute_h
-
-#include "commandType.h"
-
-// Executes and handles the chaining of commands via pre-existing pipe forking and IO redirection. 
-void commandExecute (struct commandType* command, int* pipes, int input_fd);
-
-#endif
