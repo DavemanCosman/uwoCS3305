c7d730869d585748d791b2f95d4163d7762b67a4
David Cosman <dcosman@uwo.ca>
2016-02-08 12:34:57 -0500

    Update commandHistory.h

--- Asn1/commandHistory.h
+++ Asn1/commandHistory.h
@@ -5,15 +5,15 @@
 void init_commandHistory();
 
 // Create space and copy command in history memory
-char* create_commandhistory_entry(char* command);
+char* create_commandHistory_entry(char* command);
 
 // Add command to commandHistory
-void add_commandhistory_entry(char* history_item);
+void add_commandHistory_entry(char* history_item);
 
 // Print last 10 (or less) history commands (if any)
-void print_commandhistory();
+void print_commandHistory();
 
 // Free up memory taken by history
-void free_history();
+void free_commandHistory();
 
 #endif
