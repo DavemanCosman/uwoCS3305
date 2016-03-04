292fd23083f6b312df16bec2e1626c59b869a59a
David Cosman <dcosman@uwo.ca>
2016-03-03 20:05:50 -0500

    Update compare.c added minimal function call

--- Asn2/compare.c
+++ Asn2/compare.c
@@ -20,25 +20,38 @@
 uint64_t diff(struct timespec start, struct timespec end)
 {
   uint64_t diff;
-
   diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
-
   return diff;
 }
 
+void minimalFunc(){}
+
+/*int main(void)
+{
+  start = clock();
+  minimalFunc();
+  end = clock();
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("Time taken for function call :\n%.17f\n",cpu_time_used);
+
+  start = clock();
+  getpid();
+  end = clock();
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("Time taken for system call :\n%.17f\n",cpu_time_used);
+}*/
 
 int main(int argc, char *argv[])
 {
+  start = clock():
+  funcCall();
   struct timespec start, end;
   uint64_t elapsedTime;
   int i,s;
 
-
   clock_gettime(CLOCK_ID, &start);
-     for (i=0; i < 10; i++)
-        s = 0;
+  minimalFunc();
   clock_gettime(CLOCK_ID, &end);
   elapsedTime = diff(start,end);
-  printf("elapsed process CPU time is %llu\n",(long long unsigned int) elapsedTime);
-
+  printf("elapsed process CPU time for Minimal Function Call is %llu\n",(long long unsigned int) elapsedTime);
 }
