d7d0b632c7ddb9d986793f67052209e343524c76
David Cosman <dcosman@uwo.ca>
2016-03-03 21:17:49 -0500

    Update kernelInfo.c

--- Asn2/kernelInfo.c
+++ Asn2/kernelInfo.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/utsname.h>
 
-int main() {
-    system("cat /proc/cpuinfo | grep processor");
-    system("cat /proc/cpuinfo | grep vendor_id");
-    system("cat /proc/version");
-    return 0;
+main() {
+
+   FILE *fp;
+   char buff[255];
+
+   fp = fopen("/proc/cpuinfo", "rb");
+   fscanf(fp, "%s", buff);
+   printf("1 : %s\n", buff );
+
+   fgets(buff, 255, (FILE*)fp);
+   printf("2: %s\n", buff );
+   
+   fgets(buff, 255, (FILE*)fp);
+   printf("3: %s\n", buff );
+   fclose(fp);
 }
