85ee599a60f18d83634997fe23dc1fb85ef5c3b1
David Cosman <dcosman@uwo.ca>
2016-03-07 15:06:24 -0500

    Update comparison1.c

--- Asn2/comparison1.c
+++ Asn2/comparison1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <math.h>
 
+int main(int argc, char *argv[])
+{
+  if (argc != 2){
+       perror("incorrect arguments");
+       exit(0);
+    }
+  
+  char command[50];
+  strcpy(command, argv[1]);
+
+  while (1){
+    sleep(1);
+    system(command);
+  }
+
+}
